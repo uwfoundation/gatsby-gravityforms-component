fdb3f7eb1b59355a3c7f234f9b97f329be724c81
Sarah Loach <sarah.loach@supportuw.org>
2022-03-09 15:31:23 -0600

    remove phone length validation for now

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,7 +6,6 @@ import InputWrapper from '../InputWrapper'
 import InputSubfieldWrapper from '../InputSubfieldWrapper'
 import 'react-phone-number-input/style.css'
 import PhoneInputWithCountry from 'react-phone-number-input/react-hook-form'
-import { isPossiblePhoneNumber } from 'react-phone-number-input'
 import { useFormContext } from 'react-hook-form'
 
 const standardType = (type) => {
@@ -158,7 +157,6 @@ const Input = ({ fieldData, name, value, fieldHidden, subfield, fromNameField, .
                   onChange={setPhoneValue}
                   rules={{
                       required: !fieldHidden ? isRequired : false,
-                      validate: (e) => isPossiblePhoneNumber(String(e)),
                   }}
                   />) 
                   : (<input
