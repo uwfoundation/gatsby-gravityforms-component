602e466c0bbb6b939de86191e7518de4f5b9ef4b
Sarah Loach <80062119+sarah-wfaa@users.noreply.github.com>
2023-12-01 17:24:05 -0600

    put custom maxLength first if it exists

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -171,20 +171,20 @@ const Input = ({ fieldData, name, value, fieldHidden, subfield, fromNameField, .
                     size
                 )}
                 id={name}
-                maxLength={type === 'phone' ? 26 : maxLength ? maxLength : type === 'text' ? 256 : 524288} // 524288 = 512kb, avoids invalid prop type error if maxLength is undefined.
+                maxLength={maxLength ? maxLength : type === 'phone' ? 26 : type === 'text' ? 256 : 524288} // 524288 = 512kb, avoids invalid prop type error if maxLength is undefined.
                 name={name}
                 placeholder={placeholder}
                 {...register(name, {
                     value: defaultValue,
                     required: !fieldHidden ? isRequired && strings.errors.required : false,
-                    maxLength: type === 'phone' ? {
-                        value: 25,
-                        message: 'Phone number must be 25 characters or less.',
-                    } : maxLength > 0 && maxLength ? {
+                    maxLength: maxLength > 0 && maxLength ? {
                         value: maxLength > 0 && maxLength,
                         message:
                             maxLength > 0 &&
                             `${strings.errors.maxChar.front}  ${maxLength} ${strings.errors.maxChar.back}`,
+                    } : type === 'phone' ? {
+                        value: 25,
+                        message: 'Phone number must be 25 characters or less.',
                     } : type === 'text' ? {
                         value: 255,
                         message: "Must be 255 characters or less.",
